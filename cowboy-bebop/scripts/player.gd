ac578ccb2b7178733bf315b7c596fdd4085c8316
vinct <vincentkanagasundaram@gmail.com>
2025-01-24 18:38:55 +0100

    Add better character movement

--- cowboy-bebop/scripts/player.gd
+++ cowboy-bebop/scripts/player.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
 @export var speed = 150
+@onready var animatedSprite = $AnimatedSprite2D
+var last_direction = 0
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
+	
+	if input_direction == Vector2.LEFT:
+		animatedSprite.play("move_left")
+		last_direction = 1
+	elif input_direction == Vector2.RIGHT:
+		animatedSprite.play("move_right")
+		last_direction = 3
+	elif input_direction == Vector2.UP:
+		animatedSprite.play("move_up")
+		last_direction = 2
+	elif input_direction == Vector2.DOWN:
+		animatedSprite.play("move_down")
+		last_direction = 0
+	else:
+		if (last_direction == 0): animatedSprite.play("idle_down")
+		elif (last_direction == 1): animatedSprite.play("idle_left")
+		elif (last_direction == 2): animatedSprite.play("idle_up")
+		else: animatedSprite.play("idle_right")		
+		print(last_direction)
+	
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
